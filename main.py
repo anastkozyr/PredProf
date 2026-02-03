f253cd3f28a382b770cca5b8f4c10e46f6c7d827
voroninav12 <voronina_veronika12072009@mail.ru>
2026-02-03 10:01:03 +0300

    базовая пятерочка готова!

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -21,9 +22,11 @@ def load_user(user_id):
     finally:
         db_sess.close()
 
+
 @app.route('/')
 def start_page():
     return render_template('base.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -34,13 +37,13 @@ def register():
         try:
             if db_sess.query(User).filter(User.name == form.name.data).first():
                 return render_template('register.html',
-                                     form=form,
-                                     message="Пользователь с таким именем уже существует")
+                                       form=form,
+                                       message="Пользователь с таким именем уже существует")
 
             user = User(
                 name=form.name.data,
                 # email больше не обязателен
-                level=form.level.data # Поле level
+                level=form.level.data  # Поле level
             )
             user.set_password(form.password.data)
 
@@ -51,6 +54,7 @@ def register():
             db_sess.close()
 
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -67,7 +71,7 @@ def login():
 
             return render_template('login.html',
                                    message="Неправильное имя пользователя или пароль",
-                                    form=form)
+                                   form=form)
         finally:
             db_sess.close()
 
@@ -80,30 +84,37 @@ def logout():
     logout_user()
     return redirect("/")
 
+
 @app.route('/images/<path:filename>')
 def images_files(filename):
     return send_from_directory('images', filename)
+
 
 @app.route('/go-to-trainer')
 @login_required
 def go_to_trainer():
     return render_template('practic.html')
 
+
 @app.route('/smartphone_basics')
 @login_required
 def smartphone_basics():
     user_level = current_user.level
     return render_template('smartphone_basics.html', user_level=user_level)
+
+
 @app.route('/smartphone_basics_base')
 @login_required
 def smartphone_basics_base():
     user_level = current_user.level
     return render_template('smartphone_basics_base.html', user_level=user_level)
 
+
 @app.route('/messenger_training')
 @login_required
 def messenger_training():
     return render_template('messenger_training.html')
+
 
 @app.route('/public-services')
 @login_required
@@ -122,10 +133,12 @@ def teory_smartphone():
 def online_shopping():
     return render_template('online_shopping_basic.html')
 
+
 @app.route('/buttons')
 @login_required
 def buttons():
     return render_template('buttons.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8028, host='127.0.0.1')
